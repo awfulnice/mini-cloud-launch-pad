8dc82a56984d30cd1711a4b2fdbd3cb50b310288
awfulnice <awfulnice@gmail.com>
2016-04-28 15:56:55 +0200

    mock-aws

---
+++ spec/router_spec.js
@@ -0,0 +1,25 @@
+//https://www.distelli.com/docs/tutorials/test-your-nodejs-with-jasmine
+var request = require("request");
+var server = require("../server.js")
+var base_url = "http://localhost:8080/"
+
+describe("middleware/router.js", function() {
+	describe("GET /", function() {
+
+		it("returns status code 200", function(done) {
+			request.get(base_url, function(error, response, body) {
+				expect(response.statusCode).toBe(200);
+				server.closeServer();
+				done();
+			});
+		});
+
+//		it("returns Hello World", function(done) {
+//			request.get(base_url, function(error, response, body) {
+//				expect(body).toBe("Hello World");
+//				server.closeServer();
+//				done();
+//			});
+//		});
+	});
+});
